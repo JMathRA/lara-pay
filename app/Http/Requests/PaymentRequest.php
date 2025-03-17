e498a514d58babba8273fcd3bfac4f11704cbd4a
JMathRA <j.matheusalbuquerque1@gmail.com>
2025-03-17 19:26:31 -0300

    first commit

---
+++ app/Http/Requests/PaymentRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'value' => ['required'],
+            'name' => ['required'],
+            'cpfCnpj'=> ['required'],
+        ];
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function messages(): array
+    {
+        return [
+            'value' => ['Digite o valor'],
+            'name' => ['Digite o nome'],
+            'cpfCnpj'=> ['Digite o CPF ou CNPJ'],
+        ];
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PaymentController;
+use Faker\Provider\ar_EG\Payment;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('payment')->group(function () {
+        Route::get('/create-boleto', [PaymentController::class, 'createBoleto'])->name('payment.create-boleto');
+        Route::post('/validate', [PaymentController::class, 'validatePayment'])->name('payment.validate');
+    });
+});
+
+require __DIR__.'/auth.php';
